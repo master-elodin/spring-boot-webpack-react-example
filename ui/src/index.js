c0745162d2dfad7aba91911cfd4af7d04dffcdae
master-elodin <timothy.vandoren@gmail.com>
2018-06-03 14:50:15 -0500

    UI now requests data from server

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,12 @@
 var jQuery = require("jquery-ajax");
 
 jQuery.ajax({
-    url: 'localhost:8080/greeting',
+    url: './greeting',
     method: 'GET'
 }).then((data) => {
     var element = document.createElement('div');
     
-    element.innerHTML = "Hello world! I've received the following message from the server: " + data;
+    element.innerHTML = `<h2>I've received the following message from the server:</h2><br/><h2 style='font-family: monospace'>${data}</h2>`;
 
     document.body.appendChild(element);
 });
